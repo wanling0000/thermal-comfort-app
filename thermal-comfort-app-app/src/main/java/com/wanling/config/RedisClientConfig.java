13113fde4623b7d9b75c8ad1724030582fd88901
Wanling Fu <wanlingfu3@gmail.com>
2025-07-31 00:15:30 +0100

    fix: redissonClient

--- thermal-comfort-app-app/src/main/java/com/wanling/config/RedisClientConfig.java
+++ thermal-comfort-app-app/src/main/java/com/wanling/config/RedisClientConfig.java
@@ -15,6 +15,7 @@ import org.redisson.client.protocol.Encoder;
 import org.redisson.client.protocol.Decoder;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -30,6 +31,7 @@ import org.springframework.context.annotation.Configuration;
 public class RedisClientConfig {
 
     @Bean("redissonClient")
+    @ConditionalOnProperty(prefix = "redisson", name = "enabled", havingValue = "true")
     public RedissonClient redissonClient(ConfigurableApplicationContext applicationContext, RedisClientConfigProperties properties) {
         Config config = new Config();
         // 根据需要可以设定编解码器；https://github.com/redisson/redisson/wiki/4.-%E6%95%B0%E6%8D%AE%E5%BA%8F%E5%88%97%E5%8C%96
